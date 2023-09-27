790ed45248efabb756431aca65abbe169abdbf87
Zoe Farrell <zoe@turing.edu>
2023-09-27 17:13:19 -0600

    Set up lab for students

--- IssueSubmitter/Controllers/GitHubController.cs
+++ IssueSubmitter/Controllers/GitHubController.cs
@@ -22,21 +22,8 @@ namespace IssueSubmitter.Controllers
         [HttpPost("createissue")]
         public async Task<IActionResult> CreateIssueAsync(GitHubIssue issue)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest("The data submitted for creating this issue is invalid");
-            }
-
-            var result = await _gitHubService.CreateIssueAsync(issue.Title, issue.Body);
-
-            if (result)
-            {
-                return Ok("Your issue has been successfully recorded!");
-            }
-            else
-            {
-                return BadRequest("An error occurred while creating the issue.");
-            }
+            //TODO: Create issue
+            return Ok("Your issue has been successfully recorded!");
         }
     }
 }
--- IssueSubmitter/Services/GitHubService.cs
+++ IssueSubmitter/Services/GitHubService.cs
@@ -7,39 +7,18 @@ namespace IssueSubmitter.Services
 {
     public class GitHubService : IGitHubService
     {
+        // Using _ here because that's the convention for private variables
         private readonly HttpClient _httpClient;
 
         public GitHubService()
         {
-            _httpClient = new HttpClient() { BaseAddress = new Uri("https://api.github.com") };
-            _httpClient.DefaultRequestHeaders.UserAgent.Add(
-                new ProductInfoHeaderValue("IssueSubmitter", "1.0")
-            );
-            _httpClient.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json")
-            );
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                "Bearer",
-                "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"
-            );
+            //TODO Create Client
         }
 
         public async Task<bool> CreateIssueAsync(string title, string body)
         {
-            var repoOwner = "zoefarrell";
-            var repoName = "MvcMovieStarter";
-            var jsonContent = new StringContent(
-                $"{{\"title\":\"{title}\",\"body\":\"{body}\"}}",
-                Encoding.UTF8,
-                "application/json"
-            );
-
-            var response = await _httpClient.PostAsync(
-                $"/repos/{repoOwner}/{repoName}/issues",
-                jsonContent
-            );
-
-            return response.IsSuccessStatusCode;
+            //TODO Create issue
+            return true;
         }
     }
 }
